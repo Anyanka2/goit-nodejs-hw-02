3a94441f6c5a093ac00074adaec34fb90d74b774
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-11-08 12:11:05 +0100

    added possibility to add and change avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const validateBody = require("../../middlewares/validateBody.js");
 const authenticate = require("../../middlewares/authenticate.js");
+const upload = require("../../middlewares/upload.js");
 const ctrl = require("../../controllers/auth.js");
 
 const authValidation = require("../../validation/auth.js");
@@ -18,5 +19,7 @@ router.post("/login", validateBody(authValidation.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
